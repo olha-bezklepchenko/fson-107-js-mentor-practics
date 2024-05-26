091fa523f3b99691892497eb8fea4447f9587d0d
Olha Bezklepchenko <krolichek.11@gmail.com>
2024-05-26 13:52:47 +0200

    task1 task2

---
+++ js/lesson01.js
@@ -0,0 +1,35 @@
+//* Розгалуження: if, if...else, else...if
+//TODO: 1 ===================================
+//Даний рядок, що складається із символів, наприклад, 'abcde'.
+// Перевірте, що першим символом цього рядка є буква 'a'.
+// Якщо це так - виведіть 'так', інакше виведіть 'ні'.
+// const string = "abcde";
+// console.log(string[0]);
+// if (string[0] === "a") {
+//   console.log("yes");
+// } else {
+//   console.log("no");
+// }
+
+// string.startsWith("a") ? console.log("yes") : console.log("no");
+
+//TODO: 2 ===================================
+// Якщо ваше ім’я містить більше 4 букв і прізвище більше 5, то виведіть на екран число, яке дорівнює сумі букв імені і прізвища.
+// Якщо умова не виконується - вивести “неможливо виконати умову”.
+// Наприклад: “Віктор Сташко” Віктор - 6 літер, Сташко - 6 літер. Виводимо 12.
+// Використовувати вбудований метод length.
+
+// const firstName = "Vic";
+// const lastName = "Stashko";
+// const firstNameLength = firstName.length;
+// const lastNameLength = lastName.length;
+
+// if (firstNameLength > 4 && lastNameLength > 5) {
+//   console.log(firstNameLength + lastNameLength);
+// } else {
+//   console.log("неможливо виконати умову");
+// }
+
+// firstNameLength > 4 && lastNameLength > 5
+//   ? console.log(firstNameLength + lastNameLength)
+//   : console.log("неможливо виконати умову");
