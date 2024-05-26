f946cc2a803929dac05bca772bb5cca83a594046
Anna Velychko <162054651+Annvel126V@users.noreply.github.com>
2024-05-26 14:38:50 +0200

    task-3, task-4

--- js/lesson01.js
+++ js/lesson01.js
@@ -33,3 +33,37 @@
 // firstNameLength > 4 && lastNameLength > 5
 //   ? console.log(firstNameLength + lastNameLength)
 //   : console.log("неможливо виконати умову");
+
+
+
+
+//TODO:============task-3=========================
+// Написати скрипт, який перевіряє чи введене число через prompt потрапляє в діапазон від 55 до 99 включно.
+// Якщо так, вивести в консоль "Число потрапляє в діапазон".
+// Якщо ні, вивести в консоль "Число не потрапляє в діапазон".
+// function checkNumber() {
+//     const number = prompt('введіть число') 
+//    return number >= 55 && number <= 99 ? "Число потрапляє в діапазон" : "Число не потрапляє в діапазон" ;
+        
+// }
+
+// console.log(checkNumber()); 
+
+
+
+
+//TODO:============task-4=========================
+// Є 3 вікові групи : діти (0-16), дорослі(17-60), пенсіонери (61-100).
+// До якої групи відноситься Максим, якщо йому 55 років.Назву групи вивести на екран.
+
+const ageMaxsim = 10 ;
+if (ageMaxsim >= 0 &&  ageMaxsim <= 16) {
+    console.log("child");
+} else if (ageMaxsim >= 17 && ageMaxsim <= 60) {
+    console.log("adault");
+} else if (ageMaxsim >= 61 && ageMaxsim <= 100) { 
+console.log("old peopel") 
+    
+} else {
+    console.log("erro");
+}
