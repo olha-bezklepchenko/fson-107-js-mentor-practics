d8abc3684c912e6c3b4a1770d34eeb90bcc7df2f
Anna Velychko <162054651+Annvel126V@users.noreply.github.com>
2024-07-07 14:26:20 +0200

    add #2

--- js/lesson04.js
+++ js/lesson04.js
@@ -6,7 +6,7 @@ const boxes = 150;
 const randomNumber = () => Math.floor(Math.random() * 100) + 1;
 
 const divContainer = document.createElement("div");
-console.log(divContainer);
+// console.log(divContainer);
 divContainer.classList.add("number-container");
 
 for (let i = 0; i < boxes; i++) {
@@ -20,7 +20,7 @@ for (let i = 0; i < boxes; i++) {
   divContainer.appendChild(box);
 }
 
-console.log(divContainer);
+// console.log(divContainer);
 
 // bodyEl.after(divContainer);
 // bodyEl.before(divContainer);
@@ -28,3 +28,54 @@ console.log(divContainer);
 bodyEl.prepend(divContainer);
 
 // bodyEl.insertAdjacentElement("afterbegin", divContainer);
+
+
+
+
+//TODO:=========task-02=================
+// Створи HTML список фільмів на основі масиву під назвою films. Користувач обожнює дивитися кіно, дивиться його часто, тому при вході на сторінку він хоче бачити, які фільми вже були переглянуті, а до яких він ще не добрався. Вперу чергу зарендери фільми на сторінку, використовуй допоміжну функцію createMarkup(), яка буде повертати розмітку. Додай розмітку на сторінку задопомогою insertAdjacentHTML() і тільки після цього зроби елементи фільмів (li), які вже були переглянуті напівпрозорими (opacity: 0.5). Для цього використовуй масив унікальних id фільмів, знайди на сторінці елементи у яких id дорівнює тому id фільма, який користувач вже перелянув аби саме їх зробити напівпрозорими.
+
+const films = [
+  {
+    title: 'Tetris',
+    imgUrl: 'https://static.hdrezka.ac/i/2023/3/20/f509264b419fdmu53x38j.jpg',
+    id: 'film_1',
+  },
+  {
+    title: 'Avatar: The Way of Water',
+    imgUrl: 'https://static.hdrezka.ac/i/2022/12/22/tc5e6b8212683gn66r84s.jpg',
+    id: 'film_2',
+  },
+  {
+    title: 'Operation Fortune: Ruse de guerre',
+    imgUrl: 'https://static.hdrezka.ac/i/2022/2/11/s0d53f6cf0ae0tq29m85l.jpg',
+    id: 'film_3',
+  },
+  {
+    title: 'Babylon',
+    imgUrl: 'https://static.hdrezka.ac/i/2022/12/25/z330b47a82209ww99w55a.jpg',
+    id: 'film_4',
+  },
+  {
+    title: 'The Whale',
+    imgUrl: 'https://static.hdrezka.ac/i/2023/2/24/h23d8c65d734akd89q94c.jpg',
+    id: 'film_5',
+  },
+];
+const filmList = document.querySelector(".js-film-list")
+function createMarkup(array) {
+  return array.map(film => `<li id="${film.id}">
+     <img src="${film.imgUrl}" alt="${film.title}" />
+     <p>${film.title}</p>
+</li>
+    `).join()
+}
+
+filmList.innerHTML = createMarkup(films)
+// Приклад елементу списку
+{
+  /* <li id="${id}">
+     <img src="${imgUrl}" alt="${title}" />
+     <p>${title}</p>
+</li> */
+}
