2dcd0b652eca5085512c66c252ea55b67285bbea
Oleh Purys <wenger1901@gmail.com>
2024-07-07 16:47:16 +0300

    task 02,03

--- js/lesson04.js
+++ js/lesson04.js
@@ -29,49 +29,50 @@ bodyEl.prepend(divContainer);
 
 // bodyEl.insertAdjacentElement("afterbegin", divContainer);
 
-
-
-
 //TODO:=========task-02=================
 // Створи HTML список фільмів на основі масиву під назвою films. Користувач обожнює дивитися кіно, дивиться його часто, тому при вході на сторінку він хоче бачити, які фільми вже були переглянуті, а до яких він ще не добрався. Вперу чергу зарендери фільми на сторінку, використовуй допоміжну функцію createMarkup(), яка буде повертати розмітку. Додай розмітку на сторінку задопомогою insertAdjacentHTML() і тільки після цього зроби елементи фільмів (li), які вже були переглянуті напівпрозорими (opacity: 0.5). Для цього використовуй масив унікальних id фільмів, знайди на сторінці елементи у яких id дорівнює тому id фільма, який користувач вже перелянув аби саме їх зробити напівпрозорими.
 
 const films = [
   {
-    title: 'Tetris',
-    imgUrl: 'https://static.hdrezka.ac/i/2023/3/20/f509264b419fdmu53x38j.jpg',
-    id: 'film_1',
+    title: "Tetris",
+    imgUrl: "https://static.hdrezka.ac/i/2023/3/20/f509264b419fdmu53x38j.jpg",
+    id: "film_1",
   },
   {
-    title: 'Avatar: The Way of Water',
-    imgUrl: 'https://static.hdrezka.ac/i/2022/12/22/tc5e6b8212683gn66r84s.jpg',
-    id: 'film_2',
+    title: "Avatar: The Way of Water",
+    imgUrl: "https://static.hdrezka.ac/i/2022/12/22/tc5e6b8212683gn66r84s.jpg",
+    id: "film_2",
   },
   {
-    title: 'Operation Fortune: Ruse de guerre',
-    imgUrl: 'https://static.hdrezka.ac/i/2022/2/11/s0d53f6cf0ae0tq29m85l.jpg',
-    id: 'film_3',
+    title: "Operation Fortune: Ruse de guerre",
+    imgUrl: "https://static.hdrezka.ac/i/2022/2/11/s0d53f6cf0ae0tq29m85l.jpg",
+    id: "film_3",
   },
   {
-    title: 'Babylon',
-    imgUrl: 'https://static.hdrezka.ac/i/2022/12/25/z330b47a82209ww99w55a.jpg',
-    id: 'film_4',
+    title: "Babylon",
+    imgUrl: "https://static.hdrezka.ac/i/2022/12/25/z330b47a82209ww99w55a.jpg",
+    id: "film_4",
   },
   {
-    title: 'The Whale',
-    imgUrl: 'https://static.hdrezka.ac/i/2023/2/24/h23d8c65d734akd89q94c.jpg',
-    id: 'film_5',
+    title: "The Whale",
+    imgUrl: "https://static.hdrezka.ac/i/2023/2/24/h23d8c65d734akd89q94c.jpg",
+    id: "film_5",
   },
 ];
-const filmList = document.querySelector(".js-film-list")
+const filmList = document.querySelector(".js-film-list");
 function createMarkup(array) {
-  return array.map(film => `<li id="${film.id}">
+  return array
+    .map(
+      (film) => `<li id="${film.id}">
      <img src="${film.imgUrl}" alt="${film.title}" />
      <p>${film.title}</p>
 </li>
-    `).join()
+    `
+    )
+    .join();
 }
 
-filmList.innerHTML = createMarkup(films)
+filmList.innerHTML = createMarkup(films);
 // Приклад елементу списку
 {
   /* <li id="${id}">
@@ -79,7 +80,6 @@ filmList.innerHTML = createMarkup(films)
      <p>${title}</p>
 </li> */
 }
-
 
 //TODO:=========task-03=================
 /**
@@ -89,20 +89,74 @@ filmList.innerHTML = createMarkup(films)
 
 ** 1 - При події `input`, якщо користувач ввів в поле більше 6 символів то додати клас `success`. Якщо ж символів менше аніж 6, то клас `error`*/
 
-const formElement = document.querySelector(".js-contact-form");
+// const formElement = document.querySelector(".js-contact-form");
 
-const onInput = (event) => {
-  const input = event.target;
+// const onInput = (event) => {
+//   const input = event.target;
 
-  if( input.value.trim().length > 6)  {
-    input.classList.add("success") 
-    input.classList.remove("error")
-  } else {
-    input.classList.add("error")
-    input.classList.remove("success")
-  }
-}
+//   if( input.value.trim().length > 6)  {
+//     input.classList.add("success")
+//     input.classList.remove("error")
+//   } else {
+//     input.classList.add("error")
+//     input.classList.remove("success")
+//   }
+// }
 
+// formElement.addEventListener("input", onInput)
 
-formElement.addEventListener("input", onInput) 
+// ** 2 - При події `focus` зроби перевірку на пустоту поля інпута, якщо ж поле пусте, то зроби `outline` => `'3px solid red'`, якщо при фокусі поле непусте, то `outline` => `'3px solid green'`
 
+// const inputEl = document.querySelector(".js-username-input");
+
+// inputEl.addEventListener("focus", handleFocus);
+
+// function handleFocus(event) {
+//   const input = event.target;
+
+//   if (input.value.trim().length === 0) {
+//     input.style.outline = "3px solid red";
+//   } else {
+//     input.style.outline = "3px solid green";
+//   }
+// }
+
+// ** 3 - При події `blur` зроби перевірку на пустоту поля інпута, якщо ж поле пусте, то зроби `outline` => `'3px solid red'`, якщо при фокусі поле непусте, то `outline` => `'3px solid lime'`
+
+// inputEl.addEventListener("blur", handleBlur);
+
+// function handleBlur(event) {
+//   const input = event.target;
+
+//   if (input.value.trim().length < 3) {
+//     input.style.outline = "3px solid red";
+//   } else {
+//     input.style.outline = "3px solid lime";
+//   }
+// }
+
+// const formEl = document.querySelector(".js-contact-form");
+
+// formEl.addEventListener("submit", handleSubmit);
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+
+// const input = event.target.elements.userName;
+// console.log(input);
+
+// const checkbox = event.target.elements.accept.checked;
+// console.log(checkbox);
+
+// const { userName, accept } = event.target.elements;
+
+// if (!userName.value.trim() || !accept.checked) {
+//   return alert("Fields is empty");
+// }
+
+// console.log({
+//   userName: userName.value.trim(),
+// });
+
+// event.target.reset();
+// }
